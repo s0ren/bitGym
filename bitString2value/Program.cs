67184fbc435d3699d8c8fa904a19d981b6d8dde9
Søren Magnusson <smag@tec.dk>
2019-10-11 12:35:17 +0200

    Add project files.

---
+++ bitString2value/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bitString2value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
